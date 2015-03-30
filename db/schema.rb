70f0fdbbcbf131a885e53c3c21ec5a603d7597e1
crystal <crystalcontrerasgrossman@gmail.com>
2015-03-30 15:53:13 -0700

    add validation spec for comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150330170004) do
+ActiveRecord::Schema.define(version: 20150330223458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "body"
+    t.integer  "user_id"
+    t.integer  "tweet_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.string  "body"
