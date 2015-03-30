25702a7ab77a0a6605e0f274c4576f5f68de4762
crystal <crystalcontrerasgrossman@gmail.com>
2015-03-30 16:28:34 -0700

    Have README.md

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,20 @@ class CommentsController < ApplicationController
     @comment = Comment.new
   end
 
+  def create
+    @user = User.find(current_user.id)
+    @comment = @tweet.comments.new(comment_params)
+    if @tweet.save
+      flash[:success] = "Tweet Successfully Tweeted!"
+      redirect_to tweets_path
+    else
+      flash[:danger] = "There was a problem creating your tweet, please try again"
+      render :new
+    end
+  end
 
+  private
 
+  def comment_params
+    params.require(:comment).permit(:body, :user_id, :tweet_id)
 end
